569f084a0f10b2ccc2a827531541c154636d87cc
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-11-13 13:54:57 -0300

    defining prismaService to use throughout the application

--- libs/core/src/core.module.ts
+++ libs/core/src/core.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoreService } from './core.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   providers: [CoreService],
   exports: [CoreService],
+  imports: [PrismaModule],
 })
 export class CoreModule {}
