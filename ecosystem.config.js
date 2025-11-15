381e20ad17632a1133bf4d3b04a5854786a740a1
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-11-15 13:56:05 -0300

    configuring for production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,31 @@ module.exports = {
       name: 'client-partner',
       script: './dist/apps/client-partner/main.js',
       exec_mode: 'cluster',
-      instances: 1
+      instances: 2,
+      env: {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.CLIENTPARTNERPORT
+      }
     },
     {
       name: 'platform-partner',
       script: './dist/apps/platform-partner/main.js',
       exec_mode: 'cluster',
-      instances: 1
+      instances: 2,
+      env: {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.PLATFORMPARTNER
+      }
     },
     {
       name: 'moderator-partner',
       script: './dist/apps/moderator-partner/main.js',
       exec_mode: 'cluster',
-      instances: 1
+      instances: 2,
+      env: {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.MODERATORPARTNER
+      }
     },
   ],
 };
